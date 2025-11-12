4cbf8ff10a208be0c80d2fc1d809de1a00bbcd2e
Diego <dpadula@epe.santafe.gov.ar>
2025-11-12 14:58:03 -0300

    Test push hacia dos repos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import siteRoutes from './routes/site.routes.js';
 
-//Para hacer test de push (ver los diferentes remotes)
 const app = express();
 
 // Middlewares base
